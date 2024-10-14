16247602a8fbad77b434d22af3dc2e3a594894a0
Hank Shen <swh@admpub.com>
2024-10-14 16:12:22 +0800

    update

--- library/httpserver/middleware.go
+++ library/httpserver/middleware.go
@@ -23,7 +23,7 @@ func MaxRequestBodySize(h echo.Handler) echo.HandlerFunc {
 func Transaction() echo.MiddlewareFunc {
 	return func(h echo.Handler) echo.Handler {
 		return echo.HandlerFunc(func(c echo.Context) error {
-			c.SetTransaction(factory.NewParam())
+			c.SetTransaction(factory.NewParam().SetContext(c))
 			return h.Handle(c)
 		})
 	}
