e612e8831d102ef49c4a1b4c7941112d45f287d5
Hank Shen <swh@admpub.com>
2024-11-20 17:34:07 +0800

    update

--- registry/dashboard/init.go
+++ registry/dashboard/init.go
@@ -1,9 +1,14 @@
 package dashboard
 
 import (
+	"html/template"
+
+	"github.com/coscms/webcore/library/backend"
 	"github.com/coscms/webcore/library/dashboard"
 	"github.com/coscms/webcore/library/httpserver"
+	"github.com/coscms/webcore/library/notice"
 	"github.com/coscms/webcore/model"
+	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
 )
 
@@ -18,7 +23,12 @@ func init() {
 		//用户统计
 		userMdl := model.NewUser(ctx)
 		userCount, _ := userMdl.Count(nil)
-		return userCount
+		onlineUsers := notice.OnlineUserCount()
+		user := backend.User(ctx)
+		if user != nil && !notice.IsOnline(user.Username) {
+			onlineUsers++
+		}
+		return template.HTML(com.String(userCount) + ` <a class="label label-success" href="` + backend.URLFor(`/manager/user`) + `?online=Y">` + ctx.T(`在线`) + `:` + com.String(onlineUsers) + `</a>`)
 	}))
 	httpserver.Backend.Dashboard.TopButtons.Add(0,
 		&dashboard.Button{
