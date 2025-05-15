7ec70f4c8622b7b9f2d4e60c060107932be5285c
Hank Shen <swh@admpub.com>
2025-05-15 14:29:48 +0800

    update

--- library/s3manager/s3manager_test.go
+++ library/s3manager/s3manager_test.go
@@ -7,16 +7,22 @@ import (
 	"testing"
 
 	"github.com/admpub/godotenv"
+	"github.com/admpub/pp"
 	"github.com/coscms/webcore/dbschema"
 	"github.com/coscms/webcore/library/s3manager/s3client"
 	"github.com/stretchr/testify/assert"
+	"github.com/webx-top/com"
 	"github.com/webx-top/echo"
+	"github.com/webx-top/echo/defaults"
 )
 
 func TestStat(t *testing.T) {
 	return
 	projectDir := filepath.Join(echo.Wd(), `../../../`)
 	envFile := filepath.Join(projectDir, `.env`)
+	if !com.FileExists(envFile) {
+		envFile = `.env`
+	}
 	err := godotenv.Overload(envFile)
 	if err != nil {
 		panic(err)
@@ -31,9 +37,15 @@ func TestStat(t *testing.T) {
 		Baseurl:  os.Getenv(`S3_BASEURL`),
 	}
 	mgr := s3client.New(cfg, 1024000)
-	exists, err := mgr.Exists(context.Background(), ``)
+	exists, err := mgr.Exists(context.Background(), `/not-exists`)
 	if err != nil {
 		panic(err)
 	}
 	assert.Equal(t, false, exists)
+	ctx := defaults.NewMockContext()
+	dirs, _, err := mgr.List(ctx, `/`)
+	if err != nil {
+		panic(err)
+	}
+	pp.Println(dirs)
 }
