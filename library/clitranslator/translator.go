3e4891cd1b41425555d2382217c1b1a2a5d8eada
Hank Shen <swh@admpub.com>
2025-08-20 14:32:47 +0800

    update

--- library/clitranslator/translator.go
+++ library/clitranslator/translator.go
@@ -2,7 +2,6 @@ package clitranslator
 
 import (
 	"github.com/admpub/once"
-	"github.com/coscms/webcore/cmd/bootconfig"
 	"github.com/coscms/webcore/library/config"
 	"github.com/webx-top/echo/middleware/language"
 )
@@ -12,7 +11,7 @@ var translock once.Once
 var LangCode = `zh-CN` // 默认语言
 
 func initTranslate() {
-	translate = BuildTranslator(config.FromFile().Language, LangCode)
+	translate = config.FromFile().BuildTranslator(LangCode)
 }
 
 func GetTranslator() *language.Translate {
@@ -22,17 +21,4 @@ func GetTranslator() *language.Translate {
 
 func ResetTranslator() {
 	translock.Reset()
-}
-
-func NewLanguage(c language.Config) *language.Language {
-	c.SetFSFunc(bootconfig.LangFSFunc)
-	c.Reload = false
-	return language.New(&c)
-}
-
-func BuildTranslator(c language.Config, langCode string) *language.Translate {
-	tr := &language.Translate{}
-	lng := NewLanguage(c)
-	tr.Reset(langCode, lng)
-	return tr
 }
--- library/config/config.go
+++ library/config/config.go
@@ -329,27 +329,3 @@ func (c *Config) SetDefaults() *Config {
 	}
 	return c.initLanguage()
 }
-
-func (c *Config) initLanguage() *Config {
-	if len(c.Language.Default) > 0 {
-		c.Language.Default = echo.NewLangCode(c.Language.Default).Normalize()
-	}
-	if len(c.Language.Fallback) > 0 {
-		c.Language.Fallback = echo.NewLangCode(c.Language.Fallback).Normalize()
-	}
-	if len(c.Language.AllList) == 0 {
-		if len(c.Language.Default) == 0 {
-			c.Language.Default = `en`
-		}
-		c.Language.AllList = []string{c.Language.Default}
-		return c
-	}
-	for index, lang := range c.Language.AllList {
-		lang = echo.NewLangCode(lang).Normalize()
-		if index == 0 && len(c.Language.Default) == 0 {
-			c.Language.Default = lang
-		}
-		c.Language.AllList[index] = lang
-	}
-	return c
-}
