4d512abc411521970560379ca25c6df2711a635e
Hank Shen <swh@admpub.com>
2025-08-20 14:41:16 +0800

    update

--- library/config/config_translator.go
+++ library/config/config_translator.go
@@ -34,10 +34,25 @@ func (c *Config) GetTranslator(ctx echo.Context) echo.Translator {
 	return c.BuildTranslator(ctx.Lang().String())
 }
 
-func (c *Config) NewLanguage() *language.Language {
-	cfg := c.Language
+func (c *Config) CloneLanguageConfig() language.Config {
+	cfg := language.Config{
+		Project:      c.Language.Project,
+		Default:      c.Language.Default,
+		Fallback:     c.Language.Fallback,
+		AllList:      make([]string, len(c.Language.AllList)),
+		RulesPath:    make([]string, len(c.Language.RulesPath)),
+		MessagesPath: make([]string, len(c.Language.MessagesPath)),
+		Reload:       false,
+	}
+	copy(cfg.AllList, c.Language.AllList)
+	copy(cfg.RulesPath, c.Language.RulesPath)
+	copy(cfg.MessagesPath, c.Language.MessagesPath)
 	cfg.SetFSFunc(bootconfig.LangFSFunc)
-	cfg.Reload = false
+	return cfg
+}
+
+func (c *Config) NewLanguage() *language.Language {
+	cfg := c.CloneLanguageConfig()
 	return language.New(&cfg)
 }
 
