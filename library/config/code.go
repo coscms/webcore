54c77da3f3b77f4b43c8afdc6746b5041333fa4e
Hank Shen <swhbox@foxmail.com>
2025-11-22 16:23:21 +0800

    update

--- library/config/code.go
+++ library/config/code.go
@@ -3,9 +3,15 @@ package config
 import (
 	"net/http"
 
+	"github.com/coscms/webcore/library/formbuilder"
+	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/code"
+	"github.com/webx-top/echo/middleware/language"
 )
 
 func init() {
 	code.CodeDict.SetHTTPCodeToExists(code.NonPrivileged, http.StatusForbidden)
+	formbuilder.LanguagesDefaultGetter = func(ctx echo.Context) language.Config {
+		return FromFile().Language
+	}
 }
