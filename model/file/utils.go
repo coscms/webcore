82c4746964dfd511b3bdd55b6922058d1ff27d99
Hank Shen <swhbox@foxmail.com>
2026-03-31 16:34:26 +0800

    update

--- model/file/utils.go
+++ model/file/utils.go
@@ -8,6 +8,7 @@ import (
 	"github.com/coscms/go-imgparse/imgparse"
 	"github.com/coscms/webcore/dbschema"
 	"github.com/webx-top/echo"
+	"github.com/webx-top/echo/code"
 )
 
 func ParseMime(m *dbschema.NgingFile, forceReset bool) {
@@ -37,4 +38,25 @@ func ParseImage(m *dbschema.NgingFile, reader io.Reader, forceReset bool) error 
 		m.Dpi = 0
 	}
 	return nil
+}
+
+func MakeImageWidthAndHeightChecker(minWidth, maxWidth, minHeight, maxHeight uint) func(ctx echo.Context, nf *dbschema.NgingFile) error {
+	return func(ctx echo.Context, nf *dbschema.NgingFile) error {
+		if nf.Type != `image` {
+			return nil
+		}
+		if minWidth > 0 && nf.Width < minWidth {
+			return ctx.NewError(code.DataFormatIncorrect, `图片宽度不能小于%d像素`, minWidth)
+		}
+		if maxWidth > 0 && nf.Width > maxWidth {
+			return ctx.NewError(code.DataFormatIncorrect, `图片宽度不能大于%d像素`, maxWidth)
+		}
+		if minHeight > 0 && nf.Height < minHeight {
+			return ctx.NewError(code.DataFormatIncorrect, `图片高度不能小于%d像素`, minHeight)
+		}
+		if maxHeight > 0 && nf.Height > maxHeight {
+			return ctx.NewError(code.DataFormatIncorrect, `图片高度不能大于%d像素`, maxHeight)
+		}
+		return nil
+	}
 }
