c47254cc50d4c59f2f4885b96512d61aeccb3268
Hank Shen <swhbox@foxmail.com>
2026-04-07 20:02:28 +0800

    update

--- library/cron/jobs.go
+++ library/cron/jobs.go
@@ -1,9 +1,29 @@
 package cron
 
+import (
+	"sort"
+
+	"github.com/webx-top/echo"
+)
+
 var (
 	// systemJobs 系统Job
 	systemJobs = map[string]Jobx{}
 )
+
+func ListSystemJobs() echo.KVList {
+	kvList := echo.KVList{}
+	names := make([]string, 0, len(systemJobs))
+	for name := range systemJobs {
+		names = append(names, name)
+	}
+	sort.Strings(names)
+	for _, name := range names {
+		sj := systemJobs[name]
+		kvList = append(kvList, echo.NewKV(name, sj.Description).SetHKV(`example`, sj.Example))
+	}
+	return kvList
+}
 
 func Register(name string, fn RunnerGetter, example string, description string) {
 	AddSystemJob(name, fn, example, description)
