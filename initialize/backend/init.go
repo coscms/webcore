badde1beca481f9cb47f99485c15fb0f018539f1
Hank Shen <swhbox@foxmail.com>
2026-04-17 17:19:04 +0800

    update

--- initialize/backend/init.go
+++ initialize/backend/init.go
@@ -1,11 +1,14 @@
 package backend
 
 import (
+	"net/url"
 	"os"
 
 	"github.com/coscms/webcore/cmd/bootconfig"
 	"github.com/coscms/webcore/library/config"
+	"github.com/coscms/webcore/library/httpserver"
 	"github.com/coscms/webcore/registry/route"
+	"github.com/webx-top/echo/subdomains"
 )
 
 func init() {
@@ -20,4 +23,39 @@ func init() {
 		SetPrefix(prefix)
 	}
 	bootconfig.OnStart(0, start)
+
+	config.OnKeySetSettings(`base.backendURL`, onChangeBackendURL)
+}
+
+func onChangeBackendURL(diff config.Diff) error {
+	if !bootconfig.IsWeb() || !diff.IsDiff {
+		return nil
+	}
+	oldURL, ok := diff.Old.(string)
+	if !ok {
+		return nil
+	}
+	newURL, ok := diff.New.(string)
+	if !ok {
+		return nil
+	}
+	if len(newURL) > 0 {
+		u, err := url.Parse(newURL)
+		if err != nil {
+			return err
+		}
+		if len(u.Host) > 0 {
+			subdomains.Default.Add(httpserver.KindBackend+`@`+u.Host, httpserver.Backend.Router.Echo())
+		}
+	}
+	if len(oldURL) > 0 {
+		u, err := url.Parse(oldURL)
+		if err != nil {
+			return err
+		}
+		if len(u.Host) > 0 {
+			subdomains.Default.RemoveHost(u.Host)
+		}
+	}
+	return nil
 }
